622a62ea69b5788daf974a43c7111d4101297064
Ben Smith <binji@chromium.org>
2015-08-31 22:15:22 -0700

    basic tokenizer

--- sexpr-wasm.c
+++ sexpr-wasm.c
@@ -1,6 +1,169 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TABS_TO_SPACES 8
+
+typedef enum TokenType {
+  TOKEN_TYPE_EOF,
+  TOKEN_TYPE_OPEN_PAREN,
+  TOKEN_TYPE_CLOSE_PAREN,
+  TOKEN_TYPE_ATOM,
+} TokenType;
+
+typedef struct SourceLocation {
+  const char* pos;
+  int line;
+  int col;
+} SourceLocation;
+
+typedef struct SourceRange {
+  SourceLocation start;
+  SourceLocation end;
+} SourceRange;
+
+typedef struct Token {
+  TokenType type;
+  SourceRange range;
+} Token;
+
+typedef struct Source {
+  const char* start;
+  const char* end;
+} Source;
+
+typedef struct Tokenizer {
+  Source source;
+  SourceLocation loc;
+} Tokenizer;
+
+static Token read_token(Tokenizer* t) {
+  Token result;
+
+  while (t->loc.pos < t->source.end) {
+    switch (*t->loc.pos) {
+      case ' ':
+        t->loc.col++;
+        t->loc.pos++;
+        break;
+
+      case '\t':
+        t->loc.col += TABS_TO_SPACES;
+        t->loc.pos++;
+        break;
+
+      case '\n':
+        t->loc.line++;
+        t->loc.col = 1;
+        t->loc.pos++;
+        break;
+
+      case ';':
+        if (t->loc.pos + 1 < t->source.end && t->loc.pos[1] == ';') {
+          /* line comment */
+          while (t->loc.pos < t->source.end) {
+            if (*t->loc.pos == '\n')
+              break;
+
+            t->loc.col++;
+            t->loc.pos++;
+          }
+        }
+        break;
+
+      case '(':
+        if (t->loc.pos + 1 < t->source.end && t->loc.pos[1] == ';') {
+          int nesting = 1;
+          t->loc.pos += 2;
+          t->loc.col += 2;
+          while (nesting > 0 && t->loc.pos < t->source.end) {
+            switch (*t->loc.pos) {
+              case ';':
+                if (t->loc.pos + 1 < t->source.end && t->loc.pos[1] == ')') {
+                  nesting--;
+                  t->loc.col++;
+                  t->loc.pos++;
+                }
+                t->loc.col++;
+                t->loc.pos++;
+                break;
+
+              case '(':
+                if (t->loc.pos + 1 < t->source.end && t->loc.pos[1] == ';') {
+                  nesting++;
+                  t->loc.col++;
+                  t->loc.pos++;
+                }
+                t->loc.col++;
+                t->loc.pos++;
+                break;
+
+              case '\t':
+                t->loc.col += TABS_TO_SPACES;
+                t->loc.pos++;
+                break;
+
+              case '\n':
+                t->loc.line++;
+                t->loc.col = 1;
+                t->loc.pos++;
+                break;
+
+              default:
+                t->loc.pos++;
+                t->loc.col++;
+                break;
+            }
+          }
+          break;
+        } else {
+          result.type = TOKEN_TYPE_OPEN_PAREN;
+          result.range.start = t->loc;
+          t->loc.col++;
+          t->loc.pos++;
+          result.range.end = t->loc;
+          return result;
+        }
+        break;
+
+      case ')':
+        result.type = TOKEN_TYPE_CLOSE_PAREN;
+        result.range.start = t->loc;
+        t->loc.col++;
+        t->loc.pos++;
+        result.range.end = t->loc;
+        return result;
+
+      default:
+        result.type = TOKEN_TYPE_ATOM;
+        result.range.start = t->loc;
+        t->loc.col++;
+        t->loc.pos++;
+        while (t->loc.pos < t->source.end) {
+          switch (*t->loc.pos) {
+            case ' ':
+            case '\t':
+            case '\n':
+            case '(':
+            case ')':
+              result.range.end = t->loc;
+              return result;
+
+            default:
+              t->loc.col++;
+              t->loc.pos++;
+              break;
+          }
+        }
+        break;
+    }
+  }
+
+  result.type = TOKEN_TYPE_EOF;
+  result.range.start = t->loc;
+  result.range.end = t->loc;
+  return result;
+}
+
 int main(int argc, char** argv) {
   if (argc < 2) {
     fprintf(stderr, "usage: %s [file.wasm]\n", argv[0]);
@@ -27,6 +190,42 @@ int main(int argc, char** argv) {
     return 1;
   }
   fclose(f);
+
+  Source source;
+  source.start = data;
+  source.end = data + fsize;
+
+  Tokenizer tokenizer;
+  tokenizer.source = source;
+  tokenizer.loc.pos = source.start;
+  tokenizer.loc.line = 1;
+  tokenizer.loc.col = 1;
+
+  while (1) {
+    Token token = read_token(&tokenizer);
+    fprintf(stderr, "[%d:%d]:[%d:%d]: ", token.range.start.line,
+            token.range.start.col, token.range.end.line, token.range.end.col);
+    switch (token.type) {
+      case TOKEN_TYPE_EOF:
+        fprintf(stderr, "EOF\n");
+        goto done;
+
+      case TOKEN_TYPE_OPEN_PAREN:
+        fprintf(stderr, "OPEN_PAREN\n");
+        break;
+
+      case TOKEN_TYPE_CLOSE_PAREN:
+        fprintf(stderr, "CLOSE_PAREN\n");
+        break;
+
+      case TOKEN_TYPE_ATOM:
+        fprintf(stderr, "ATOM: \"%.*s\"\n",
+                (int)(token.range.end.pos - token.range.start.pos),
+                token.range.start.pos);
+        break;
+    }
+  }
+done:
 
   return 0;
 }
