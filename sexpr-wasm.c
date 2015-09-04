0f1a6f3902962a56fa975d119f8276b6a056757e
Ben Smith <binji@chromium.org>
2015-09-04 12:59:57 -0700

    add option parsing

--- sexpr-wasm.c
+++ sexpr-wasm.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <errno.h>
+#include <getopt.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -117,6 +118,9 @@ typedef struct NameMemType2Pair {
   MemType mem_type;
   Type type;
 } NameMemType2Pair;
+
+static int g_verbose;
+static const char* g_filename;
 
 static NameTypePair s_unary_ops[] = {
     {"f32.neg", TYPE_F32},   {"f64.neg", TYPE_F64},   {"f32.abs", TYPE_F32},
@@ -1421,16 +1425,127 @@ static void parse(Tokenizer* tokenizer) {
   }
 }
 
-int main(int argc, char** argv) {
-  if (argc < 2) {
-    fprintf(stderr, "usage: %s [file.wasm]\n", argv[0]);
-    return 0;
+enum {
+  FLAG_VERBOSE,
+  FLAG_HELP,
+  NUM_FLAGS
+};
+
+static struct option g_long_options[] = {
+    {"verbose", no_argument, NULL, 'v'},
+    {"help", no_argument, NULL, 'h'},
+    {NULL, 0, NULL, 0},
+};
+
+STATIC_ASSERT(NUM_FLAGS + 1 == ARRAY_SIZE(g_long_options));
+
+typedef struct OptionHelp {
+  int flag;
+  const char* metavar;
+  const char* help;
+} OptionHelp;
+
+static OptionHelp g_option_help[] = {
+    {FLAG_VERBOSE, NULL, "use multiple times for more info"},
+    {NUM_FLAGS, NULL},
+};
+
+static void usage(const char* prog) {
+  printf("usage: %s [option] filename\n", prog);
+  printf("options:\n");
+  struct option* opt = &g_long_options[0];
+  int i = 0;
+  for (; opt->name; ++i, ++opt) {
+    OptionHelp* help = NULL;
+
+    int n = 0;
+    while (g_option_help[n].help) {
+      if (i == g_option_help[n].flag) {
+        help = &g_option_help[n];
+        break;
+      }
+      n++;
+    }
+
+    if (opt->val) {
+      printf("  -%c, ", opt->val);
+    } else {
+      printf("      ");
+    }
+
+    if (help && help->metavar) {
+      char buf[100];
+      snprintf(buf, 100, "%s=%s", opt->name, help->metavar);
+      printf("--%-30s", buf);
+    } else {
+      printf("--%-30s", opt->name);
+    }
+
+    if (help) {
+      printf("%s", help->help);
+    }
+
+    printf("\n");
+  }
+  exit(0);
+}
+
+static void parse_options(int argc, char** argv) {
+  int c;
+  int option_index;
+
+  while (1) {
+    c = getopt_long(argc, argv, "vh", g_long_options, &option_index);
+    if (c == -1) {
+      break;
+    }
+
+  redo_switch:
+    switch (c) {
+      case 0:
+        c = g_long_options[option_index].val;
+        if (c) {
+          goto redo_switch;
+        }
+
+        switch (option_index) {
+          case FLAG_VERBOSE:
+          case FLAG_HELP:
+            /* Handled above by goto */
+            assert(0);
+        }
+        break;
+
+      case 'v':
+        g_verbose++;
+        break;
+
+      case 'h':
+        usage(argv[0]);
+
+      case '?':
+        break;
+
+      default:
+        FATAL("getopt_long returned '%c' (%d)\n", c, c);
+        break;
+    }
   }
 
-  const char* filename = argv[1];
-  FILE* f = fopen(filename, "rb");
+  if (optind < argc) {
+    g_filename = argv[optind];
+  } else {
+    FATAL("No filename given.\n");
+    usage(argv[0]);
+  }
+}
+
+int main(int argc, char** argv) {
+  parse_options(argc, argv);
+
+  FILE* f = fopen(g_filename, "rb");
   if (!f) {
-    FATAL("unable to read %s\n", filename);
+    FATAL("unable to read %s\n", g_filename);
   }
 
   fseek(f, 0, SEEK_END);
@@ -1441,7 +1556,7 @@ int main(int argc, char** argv) {
     FATAL("unable to alloc %zd bytes\n", fsize);
   }
   if (fread(data, 1, fsize, f) != fsize) {
-    FATAL("unable to read %zd bytes from %s\n", fsize, filename);
+    FATAL("unable to read %zd bytes from %s\n", fsize, g_filename);
   }
   fclose(f);
 
