a24707f599ebd2d44ef8148614cff9b55d9b1e98
Ben Smith <binji@chromium.org>
2015-09-09 08:06:10 -0700

    use d8's readbuffer(), not read() to read file
    read() tries to interpret the data as a UTF8 string, whereas
    readbuffer() just reads the data directly into an ArrayBuffer.

--- test/wasm.js
+++ test/wasm.js
@@ -3,11 +3,6 @@ if (arguments.length != 1) {
   quit(0);
 }
 
-var data = read(arguments[0]);
-var buffer = new ArrayBuffer(data.length);
-var u8a = new Uint8Array(buffer);
-for (var i = 0; i < data.length; ++i) {
-  u8a[i] = data.charCodeAt(i);
-}
+var buffer = readbuffer(arguments[0]);
 var module = WASM.instantiateModule(buffer, {});
 print(module.main());
