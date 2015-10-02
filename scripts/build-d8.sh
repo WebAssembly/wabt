864167100b48e71ad7180855e0a8e60b696b8ea1
Ben Smith <binji@chromium.org>
2015-10-02 13:40:58 -0700

    flags for build-d8.sh script

--- scripts/build-d8.sh
+++ scripts/build-d8.sh
@@ -2,6 +2,25 @@
 set -o nounset
 set -o errexit
 
+sync=YES
+config=Release
+
+while [[ $# > 0 ]]; do
+  flag="$1"
+  case $flag in
+    --no-sync)
+      sync=NO
+      ;;
+    --debug)
+      config=Debug
+      ;;
+    *)
+      echo "unknown arg ${flag}"
+      ;;
+  esac
+  shift
+done
+
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 ROOT_DIR="$(dirname "${SCRIPT_DIR}")"
 
@@ -16,27 +35,29 @@ fi
 
 export PATH=$PWD/depot_tools:$PATH
 
-if [[ ! -d v8 ]]; then
-  echo "Fetching v8"
-  mkdir v8
+if [[ ${sync} = "YES" ]]; then
+  if [[ ! -d v8 ]]; then
+    echo "Fetching v8"
+    mkdir v8
+    pushd v8
+    fetch --no-history v8
+    ln -fs $PWD/.. v8/third_party/wasm
+    popd
+  else
+    pushd v8/v8
+    git fetch origin
+    git checkout origin/master
+    popd
+  fi
+
   pushd v8
-  fetch --no-history v8
-  ln -fs $PWD/.. v8/third_party/wasm
-  popd
-else
-  pushd v8/v8
-  git fetch origin
-  git checkout origin/master
+  gclient update
   popd
 fi
-
-pushd v8
-gclient update
-popd
 
 # Don't use the CC from the environment; v8 doesn't seem to build properly with
 # it.
 unset CC
 cd v8/v8
 GYP_GENERATORS=ninja build/gyp_v8 -Dv8_wasm=1
-time ninja -C out/Release d8
+time ninja -C out/${config} d8
