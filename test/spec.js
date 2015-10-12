2e0f15e4153f626134e2b86f79f0c089d7297be6
Katelyn Gadd <kg@luminance.org>
2015-10-12 14:46:47 -0700

    Update spec.js to expose the write() entrypoint for writing raw bytes to stdout. Suppress text for passed assertions.

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,27 @@
+function wasm_write(memory, offset, count) {
+  try {
+    var view = new Uint8Array(memory, offset, count);
+  } catch (exc) {
+    print("wasm_write(" + offset + ", " + count + ") failed on heap of size " + memory.byteLength);
+    throw exc;
+  }
+  var str = "";
+
+  for (var i = 0; i < count; i++)
+    str += String.fromCharCode(view[i]);
+
+  write(str);
+}
+
 function createModule(a) {
-  var ffi = {print: print};
+  var memory = null;
   var u8a = new Uint8Array(a);
-  print('instantiating module');
+  var ffi = {
+    print: print, 
+    write: function (offset, count) { wasm_write(memory, offset, count); }
+  };
   var module = WASM.instantiateModule(u8a.buffer, ffi);
+  memory = module.memory;
   return module;
 }
 
@@ -11,6 +30,8 @@ function startsWith(s, prefix) {
 }
 
 function runTests(module) {
+  var quiet = true;
+
   var passed = 0;
   var failed = 0;
   for (var name in module) {
@@ -19,7 +40,9 @@ function runTests(module) {
       if (startsWith(name, "$assert_return")) {
         var result = f();
         if (result == 1) {
-          print(name + " OK");
+          if (!quiet)
+            print(name + " OK");
+
           passed++;
         } else {
           print(name + " failed, expected 1, got " + result);
@@ -32,17 +55,25 @@ function runTests(module) {
         } catch (e) {
           threw = true;
         }
+
         if (threw) {
-          print(name + " OK, threw");
+          if (!quiet)
+            print(name + " OK, threw");
+
           passed++;
         } else {
           print(name + " failed, didn't throw");
           failed++;
         }
       } else if (startsWith(name, '$invoke')) {
-        print(name + " = " + f());
+        var invokeResult = f();
+
+        if (!quiet)
+          print(name + " = " + invokeResult);
       }
     }
   }
-  print(passed + "/" + (passed + failed) + " tests passed.");
+
+  if ((failed > 0) || !quiet)
+    print(passed + "/" + (passed + failed) + " tests passed.");
 }
