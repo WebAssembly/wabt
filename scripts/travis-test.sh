4f94d77153bae3e25b945a0e96bd171516742b7a
Ben Smith <binji@chromium.org>
2016-03-05 10:41:35 -0800

    .travis fixes for cmake change

---
+++ scripts/travis-test.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Copyright 2016 WebAssembly Community Group participants
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -o nounset
+set -o errexit
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+ROOT_DIR="$(dirname "${SCRIPT_DIR}")"
+BUILD_TYPES="Debug Release"
+SANITIZERS="-asan -lsan -msan"
+
+log_and_run() {
+  echo $*
+  exec $*
+}
+
+run_tests() {
+  local EXE=$1
+  if [ "${2+defined}" = "defined" ]; then
+    local ARG_FLAG="-a=$2"
+  fi
+  (cd ${ROOT_DIR} && log_and_run test/run-tests.py -e ${EXE} ${ARG_FLAG-} --timeout=10)
+}
+
+for BUILD_TYPE in ${BUILD_TYPES}; do
+  EXE=out/${CC}/${BUILD_TYPE}/sexpr-wasm
+  if [ -e ${EXE} ]; then
+    run_tests ${EXE}
+    run_tests ${EXE} --use-libc-allocator
+  fi
+
+  if [ ${CC} = "clang" ]; then
+    for SANITIZER in ${SANITIZERS}; do
+      EXE=out/${CC}/${BUILD_TYPE}/sexpr-wasm${SANITIZER}
+      if [ -e ${EXE} ]; then
+        run_tests ${EXE}
+        run_tests ${EXE} --use-libc-allocator
+      fi
+    done
+  fi
+done
