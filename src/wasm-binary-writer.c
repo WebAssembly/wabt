9db711998760a344ad26be1fe14df3862553416c
Ben Smith <binji@chromium.org>
2016-03-07 13:45:40 -0800

    rebase master; remove need for local decl fixup

--- src/wasm-binary-writer.c
+++ src/wasm-binary-writer.c
@@ -989,45 +989,44 @@ static void write_module(WasmWriteContext* ctx, WasmModule* module) {
     for (i = 0; i < module->funcs.size; ++i) {
       WasmFunc* func = module->funcs.data[i];
       print_header(ctx, "function", i);
+      /* TODO(binji): we're still remapping the locals here, but we may want to
+       have this be an option, now that it is not necessary. */
       remap_locals(ctx, func);
 
       uint8_t flags = 0;
       out_u8(ws, flags, "func flags");
       out_u16(ws, ctx->func_sig_indexes[i], "func signature index");
-      int num_locals[WASM_NUM_TYPES];
-      ZERO_MEMORY(num_locals);
-      int j;
-      for (j = 0; j < func->locals.types.size; ++j) {
-	num_locals[func->locals.types.data[j]]++;
-      }
-
       size_t func_body_offset = ctx->writer_state.offset;
       out_u16(ws, 0, "func body size");
 
-      int local_decl_count = 0;
-      size_t local_decl_count_offset = ctx->writer_state.offset;
-      out_u8(ws, 0, "local decl count");
-      if (num_locals[WASM_TYPE_I32] > 0) {
-	out_u32_leb128(ws, num_locals[WASM_TYPE_I32], "local i32 count");
-	out_u8(ws, WASM_TYPE_I32, "type i32");
-	local_decl_count++;
+      int num_locals[WASM_NUM_TYPES];
+      ZERO_MEMORY(num_locals);
+      int j;
+      for (j = 0; j < func->locals.types.size; ++j)
+        num_locals[func->locals.types.data[j]]++;
+
+      int num_local_types = 0;
+      for (j = 0; j < WASM_NUM_TYPES; ++j)
+        if (num_locals[j])
+          num_local_types++;
+
+      out_u32_leb128(ws, num_local_types, "local decl count");
+      if (num_locals[WASM_TYPE_I32]) {
+        out_u32_leb128(ws, num_locals[WASM_TYPE_I32], "local i32 count");
+        out_u8(ws, WASM_TYPE_I32, "WASM_TYPE_I32");
       }
-      if (num_locals[WASM_TYPE_I64] > 0) {
-	out_u32_leb128(ws, num_locals[WASM_TYPE_I64], "local i64 count");
-	out_u8(ws, WASM_TYPE_I64, "type i64");
-	local_decl_count++;
+      if (num_locals[WASM_TYPE_I64]) {
+        out_u32_leb128(ws, num_locals[WASM_TYPE_I64], "local i64 count");
+        out_u8(ws, WASM_TYPE_I64, "WASM_TYPE_I64");
       }
-      if (num_locals[WASM_TYPE_F32] > 0) {
-	out_u32_leb128(ws, num_locals[WASM_TYPE_F32], "local f32 count");
-	out_u8(ws, WASM_TYPE_F32, "type f32");
-	local_decl_count++;
+      if (num_locals[WASM_TYPE_F32]) {
+        out_u32_leb128(ws, num_locals[WASM_TYPE_F32], "local f32 count");
+        out_u8(ws, WASM_TYPE_F32, "WASM_TYPE_F32");
       }
-      if (num_locals[WASM_TYPE_F64] > 0) {
-	out_u32_leb128(ws, num_locals[WASM_TYPE_F64], "local f64 count");
-	out_u8(ws, WASM_TYPE_F64, "type f64");
-	local_decl_count++;
+      if (num_locals[WASM_TYPE_F64]) {
+        out_u32_leb128(ws, num_locals[WASM_TYPE_F64], "local f64 count");
+        out_u8(ws, WASM_TYPE_F64, "WASM_TYPE_F64");
       }
-      out_u8_at(ws, local_decl_count_offset, local_decl_count, "local decl count");
 
       write_func(ctx, module, func);
       int func_size =
