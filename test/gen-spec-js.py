f8cc693231149115316774eb29bd0aaf03378426
Ben Smith <binji@chromium.org>
2016-05-10 13:58:27 -0700

    update gen-spec-js.py to work with spidermonkey
    spidermonkey requires the first argument to WASM.instantiateModule be a
    TypedArray, not an ArrayBuffer.

--- test/gen-spec-js.py
+++ test/gen-spec-js.py
@@ -80,7 +80,7 @@ JS_FOOTER = """\
 function createModule(data) {
   var u8a = new Uint8Array(data);
   var ffi = {spectest: {print: print}};
-  return Wasm.instantiateModule(u8a.buffer, ffi);
+  return Wasm.instantiateModule(u8a, ffi);
 }
 
 function assertReturn(module, name, file, line) {
