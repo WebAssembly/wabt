f3fde1939f7ce537cc3d532a09b5fe10756834b2
Sam Clegg <sbc@chromium.org>
2017-01-18 14:04:34 -0800

    Run flake8 during travis builds

--- scripts/travis-before-install.sh
+++ scripts/travis-before-install.sh
@@ -18,6 +18,7 @@
 if [[ ${TRAVIS_OS_NAME} = "linux" ]]; then
   sudo apt-get update -qq
   sudo apt-get install -qq re2c bison gcc-4.8-multilib g++-4.8-multilib
+  sudo pip install flake8
 elif [[ ${TRAVIS_OS_NAME} = "osx" ]]; then
   brew update
   brew install re2c bison
