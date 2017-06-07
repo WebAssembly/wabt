2355dd3935e5945568366647c2181215823f108d
Ben Smith <binji@chromium.org>
2017-06-06 23:52:29 -0700

    Add PPA for re2c 0.16

--- scripts/travis-before-install.sh
+++ scripts/travis-before-install.sh
@@ -16,6 +16,7 @@
 #
 
 if [[ ${TRAVIS_OS_NAME} = "linux" ]]; then
+  sudo add-apt-repository ppa:jbboehr/build-deps  # For re2c 0.16.
   sudo apt-get update -qq
   sudo apt-get install -qq re2c bison gcc-4.8-multilib g++-4.8-multilib
   sudo pip install flake8
