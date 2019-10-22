99704126b2b9ee1ab973f28d93cab689b54f7075
Syrus <me@syrusakbary.com>
2019-10-22 10:44:43 -0700

    Improved WASI output

--- scripts/build_wasi.sh
+++ scripts/build_wasi.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
+# Install latest version of wasienv
+curl https://raw.githubusercontent.com/wasienv/wasienv/master/install.sh | sh
+
 mkdir -p out/wasi
 cd out/wasi
 
-# We download the SDK
-if [[ ! -d wasi-sdk-5.0 ]]; then
-    OS=$(uname | tr '[:upper:]' '[:lower:]')
-    case "$OS" in
-        darwin) WASI_URL='https://github.com/CraneStation/wasi-sdk/releases/download/wasi-sdk-5/wasi-sdk-5.0-macos.tar.gz';;
-        linux) WASI_URL='https://github.com/CraneStation/wasi-sdk/releases/download/wasi-sdk-5/wasi-sdk-5.0-linux.tar.gz';;
-        *) printf "$red> The OS (${OS}) is not supported by this installation script.$reset\n"; exit 1;;
-    esac
-    curl -L -o wasi-sdk-5.0.tar.gz ${WASI_URL}
-    tar zxvf wasi-sdk-5.0.tar.gz
-fi
-
-cmake -DCMAKE_TOOLCHAIN_FILE=../../wasi-sdk.cmake -DBUILD_TESTS=OFF ../..
+# Make the regular build, wrapping it with wasimake
+wasimake cmake -DBUILD_TESTS=OFF ../..
 make -j 4
 
 # Strip and optimize the wasm files
