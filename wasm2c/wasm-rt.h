15264cd20be3510058e56d1de5db1f32ae5364eb
Ben Smith <binji@chromium.org>
2020-06-04 15:45:02 -0700

    [wasm2c] Implement trap handler for linux/macos (#1442)
    * Test whether this works for macOS
    
    * Use __builtin_memcpy (fixes bug on macOS)
    * Use #if instead of #ifdef (so 0 and 1 values work properly)
    * Add `WASM_RT_SETJMP` and `WASM_RT_LONGJMP` macros

--- wasm2c/wasm-rt.h
+++ wasm2c/wasm-rt.h
@@ -35,6 +35,26 @@ extern "C" {
 #define WASM_RT_MAX_CALL_STACK_DEPTH 500
 #endif
 
+/** Whether to enable memory checking via a signal handler.
+ *
+ * This is usually 10%-25% faster, but requires OS-specific support.
+ * */
+#ifndef WASM_RT_MEMCHECK_SIGNAL_HANDLER
+#define WASM_RT_MEMCHECK_SIGNAL_HANDLER 1
+#endif
+
+#if WASM_RT_MEMCHECK_SIGNAL_HANDLER
+#if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+#define WASM_RT_MEMCHECK_SIGNAL_HANDLER_POSIX 1
+#else
+#error "No signal handler implementation for OS!"
+#endif
+#endif
+
+#ifndef WASM_RT_MEMCHECK_SIGNAL_HANDLER_POSIX
+#define WASM_RT_MEMCHECK_SIGNAL_HANDLER_POSIX 0
+#endif
+
 /** Reason a trap occurred. Provide this to `wasm_rt_trap`. */
 typedef enum {
   WASM_RT_TRAP_NONE,         /** No error. */
