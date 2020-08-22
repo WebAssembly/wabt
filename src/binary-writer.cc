baa1adeb66cd67a264ed2f95cc847a3f84a5aead
sammax <sammax@outlook.de>
2020-08-22 18:06:25 +0200

    blocktype relocations

--- src/binary-writer.cc
+++ src/binary-writer.cc
@@ -245,7 +245,7 @@ void BinaryWriter::WriteBlockDecl(const BlockDeclaration& decl) {
   Index index = decl.has_func_type ? module_->GetFuncTypeIndex(decl.type_var)
                                    : module_->GetFuncTypeIndex(decl.sig);
   assert(index != kInvalidIndex);
-  WriteS32Leb128(stream_, index, "block type function index");
+  WriteU32Leb128WithReloc(index, "block type function index", RelocType::TypeIndexLEB);
 }
 
 void BinaryWriter::WriteSectionHeader(const char* desc,
