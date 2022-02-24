0b1c9b236bdc71dc934c39948e3c1d5a3ec38fdf
Soni L <EnderMoneyMod@gmail.com>
2022-02-24 00:07:48 -0300

    $1 always wins

--- scripts/clang-format-diff.sh
+++ scripts/clang-format-diff.sh
@@ -3,10 +3,10 @@
 set -o errexit
 set -o pipefail
 
-if [ -n "$GITHUB_EVENT_BEFORE" ] && [ "push" = "$GITHUB_EVENT_NAME" ]; then
-  BRANCH="$GITHUB_EVENT_BEFORE"
-elif [ -n "$1" ]; then
+if [ -n "$1" ]; then
   BRANCH="$1"
+elif [ -n "$GITHUB_EVENT_BEFORE" ] && [ "push" = "$GITHUB_EVENT_NAME" ]; then
+  BRANCH="$GITHUB_EVENT_BEFORE"
 elif [ -n "$GITHUB_BASE_REF" ]; then
   BRANCH="origin/$GITHUB_BASE_REF"
 else
