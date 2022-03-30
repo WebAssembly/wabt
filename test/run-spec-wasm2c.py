d4858a7e8ea5dceccf5dbb7792abe036ad890e08
Yuhan Deng <yhdeng@stanford.edu>
2022-03-29 17:59:39 -0700

    Fix register command for named modules

--- test/run-spec-wasm2c.py
+++ test/run-spec-wasm2c.py
@@ -134,6 +134,7 @@ class CWriter(object):
         self.module_idx = 0
         self.module_name_to_idx = {}
         self.module_prefix_map = {}
+        self.idx_to_module_name = {}
         self.idx_to_c_name = {}
         self._MaybeWriteDummyModule()
         self._CacheModulePrefixes()
@@ -174,6 +175,7 @@ class CWriter(object):
 
                 if 'name' in command:
                     self.module_name_to_idx[command['name']] = idx
+                    self.idx_to_module_name[idx] = command['name']
                     self.module_prefix_map[command['name']] = name
 
                 idx += 1
@@ -185,6 +187,8 @@ class CWriter(object):
                 else:
                     name_idx = idx - 1
 
+                if name_idx in self.idx_to_module_name:
+                    self.module_prefix_map[self.idx_to_module_name[name_idx]] = name
                 self.module_prefix_map[name_idx] = name
                 c_filename = command['as'] + '.c'
                 self.idx_to_c_name[name_idx] = c_filename
