9943a8f36667f34388a6c67b56ad8638d0f8fae5
Keith Winstein <keithw@cs.stanford.edu>
2022-07-15 01:10:32 -0500

    gen-wasm2c-templates.cmake: use line-by-line string to work around MSVC limit

--- scripts/gen-wasm2c-templates.cmake
+++ scripts/gen-wasm2c-templates.cmake
@@ -1,4 +1,5 @@
 # https://stackoverflow.com/a/47801116
 file(READ ${in} content)
-set(content "const char* ${symbol} = R\"w2c_template(${content})w2c_template\";")
+string(REGEX REPLACE "(.[^\n]*\n)" "R\"w2c_template(\\1)w2c_template\"\n" content "${content}")
+set(content "const char* ${symbol} = ${content};")
 file(WRITE ${out} "${content}")
