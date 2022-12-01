22a04396b5692f4604cc72ccc999bdd7588864a2
Mitch Foley <mitchfoley@chromium.org>
2022-12-01 18:04:11 -0500

    fix -Wunused-variable warnings when building without asserts

--- src/c-writer.cc
+++ src/c-writer.cc
@@ -601,24 +601,20 @@ void CWriter::DefineImportName(const Import* import,
   import_syms_.insert(name);
   import_module_sym_map_.emplace(name, import->module_name);
   global_syms_.insert(mangled);
-  bool success =
-      global_sym_map_.emplace(name + MangleField(type), mangled).second;
-  assert(success);
+  assert(global_sym_map_.emplace(name + MangleField(type), mangled).second);
 }
 
 std::string CWriter::DefineGlobalScopeName(ModuleFieldType type,
                                            std::string_view name) {
   std::string mangled = std::string(name) + MangleField(type);
   std::string unique = DefineName(&global_syms_, StripLeadingDollar(name));
-  bool success = global_sym_map_.emplace(mangled, unique).second;
-  assert(success);
+  assert(global_sym_map_.emplace(mangled, unique).second);
   return unique;
 }
 
 std::string CWriter::DefineLocalScopeName(std::string_view name) {
   std::string unique = DefineName(&local_syms_, StripLeadingDollar(name));
-  bool success = local_sym_map_.emplace(name, unique).second;
-  assert(success);
+  assert(local_sym_map_.emplace(name, unique).second);
   return unique;
 }
 
@@ -627,8 +623,7 @@ std::string CWriter::DefineStackVarName(Index index,
                                         std::string_view name) {
   std::string unique = DefineName(&local_syms_, name);
   StackTypePair stp = {index, type};
-  bool success = stack_var_sym_map_.emplace(stp, unique).second;
-  assert(success);
+  assert(stack_var_sym_map_.emplace(stp, unique).second);
   return unique;
 }
 
