PLSysSec/wasm2c_sandbox_compiler

Always do a full roundtrip in run-roundtrip.py
Even when the result is to be printed rather than compared byte for byte
with the first version its still good to process the resulting wat
output file so that we know we can parse what we generate.

Case in point, this changed caused me to fix two latent bugs:

1. We were not correctly parsing events with inline import/export.
2. We were output element segment names even when bulk memory
   was not enabled (See #1651)

The fix for (2) is a little more involved that we might like since for
the first time the wat writer needs to know what features are enabled.

Fixes: #1651

src/wat-writer.h
@@ -18,6 +18,7 @@
 #define WABT_WAT_WRITER_H_
 
 #include "src/common.h"
+#include "src/feature.h"
 
 namespace wabt {
 
@@ -25,6 +26,9 @@ struct Module;
 class Stream;
 
 struct WriteWatOptions {
+  WriteWatOptions() = default;
+  WriteWatOptions(const Features& features) : features(features) {}
+  Features features;
   bool fold_exprs = false;  // Write folded expressions.
   bool inline_export = false;
   bool inline_import = false;

WebAssembly/wabt

include/wabt/wat-writer.h
@@ -18,6 +18,7 @@
 #define WABT_WAT_WRITER_H_
 
 #include "wabt/common.h"
+#include "wabt/feature.h"
 
 namespace wabt {
 
@@ -25,6 +26,9 @@ struct Module;
 class Stream;
 
 struct WriteWatOptions {
+  WriteWatOptions() = default;
+  WriteWatOptions(const Features& features) : features(features) {}
+  Features features;
   bool fold_exprs = false;  // Write folded expressions.
   bool inline_export = false;
   bool inline_import = false;