7eb5868cf0c37c2fe800fa92017fc063bc8fb991
Diego Frias <96022404+dzfrias@users.noreply.github.com>
2023-08-18 14:06:12 -0700

    wasm-strip: add `-R/--remove-section` option (#2282)

--- src/tools/wasm-strip.cc
+++ src/tools/wasm-strip.cc
@@ -28,6 +28,7 @@ using namespace wabt;
 static std::string s_filename;
 static std::string s_outfile;
 static std::set<std::string_view> v_sections_to_keep{};
+static std::set<std::string_view> v_sections_to_remove{};
 
 static const char s_description[] =
     R"(  Remove sections of a WebAssembly binary file.
@@ -52,15 +53,22 @@ static void ParseOptions(int argc, char** argv) {
                    [](const char* value) {
                      v_sections_to_keep.insert(std::string_view{value});
                    });
+  parser.AddOption('R', "remove-section", "SECTION NAME",
+                   "Section to specifically remove, including all the rest",
+                   [](const char* value) {
+                     v_sections_to_remove.insert(std::string_view{value});
+                   });
   parser.Parse(argc, argv);
 }
 
 class BinaryReaderStrip : public BinaryReaderNop {
  public:
   explicit BinaryReaderStrip(std::set<std::string_view> sections_to_keep,
+                             std::set<std::string_view> sections_to_remove,
                              Errors* errors)
       : errors_(errors),
         sections_to_keep_(sections_to_keep),
+        sections_to_remove_(sections_to_remove),
         section_start_(0) {
     stream_.WriteU32(WABT_BINARY_MAGIC, "WASM_BINARY_MAGIC");
     stream_.WriteU32(WABT_BINARY_VERSION, "WASM_BINARY_VERSION");
@@ -91,7 +99,12 @@ class BinaryReaderStrip : public BinaryReaderNop {
   Result BeginCustomSection(Index section_index,
                             Offset size,
                             std::string_view section_name) override {
-    if (sections_to_keep_.count(section_name) > 0) {
+    if (sections_to_remove_.count(section_name) > 0) {
+      return Result::Ok;
+    }
+
+    if (sections_to_keep_.count(section_name) > 0 ||
+        !sections_to_remove_.empty()) {
       stream_.WriteU8Enum(BinarySection::Custom, "section code");
       WriteU32Leb128(&stream_, size, "section size");
       stream_.WriteData(state->data + section_start_, size, "section data");
@@ -103,6 +116,7 @@ class BinaryReaderStrip : public BinaryReaderNop {
   MemoryStream stream_;
   Errors* errors_;
   std::set<std::string_view> sections_to_keep_;
+  std::set<std::string_view> sections_to_remove_;
   Offset section_start_;
 };
 
@@ -127,7 +141,7 @@ int ProgramMain(int argc, char** argv) {
   ReadBinaryOptions options(features, nullptr, kReadDebugNames,
                             kStopOnFirstError, kFailOnCustomSectionError);
 
-  BinaryReaderStrip reader(v_sections_to_keep, &errors);
+  BinaryReaderStrip reader(v_sections_to_keep, v_sections_to_remove, &errors);
   result = ReadBinary(file_data.data(), file_data.size(), &reader, options);
   FormatErrorsToFile(errors, Location::Type::Binary);
   if (Failed(result)) {
