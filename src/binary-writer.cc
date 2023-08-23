53b3f821c064c082d26199aca26c941603603a1a
Diego <96022404+dzfrias@users.noreply.github.com>
2023-08-22 21:09:07 -0700

    wasm2wat: write custom sections

--- src/binary-writer.cc
+++ src/binary-writer.cc
@@ -1670,6 +1670,12 @@ Result BinaryWriter::WriteModule() {
     EndSection();
   }
 
+  for (const Custom& custom : module_->customs) {
+    BeginCustomSection(custom.name.data());
+    stream_->WriteData(custom.data, "custom data");
+    EndSection();
+  }
+
   if (options_.write_debug_names) {
     std::vector<std::string> index_to_name;
 
