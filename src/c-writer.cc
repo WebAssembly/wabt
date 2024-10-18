90093f94775f5d18e979926240fa0cb19080ed97
Christian Nieves <cnieves@google.com>
2024-10-18 13:29:00 +0000

    Fix bug in wasm2c's tail call optimization codegen

--- src/c-writer.cc
+++ src/c-writer.cc
@@ -1895,7 +1895,7 @@ void CWriter::WriteTailCallWeakImports() {
     Index num_results = func.GetNumResults();
     if (num_params >= 1) {
       Write(func.decl.sig.param_types, " params;", Newline());
-      Write("wasm_rt_memcpy(params, tail_call_stack, sizeof(params));",
+      Write("wasm_rt_memcpy(&params, tail_call_stack, sizeof(params));",
             Newline());
     }
 
