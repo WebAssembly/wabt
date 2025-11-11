f645678b01c424a87b7a35d8e176ec9d52f25418
James Ring <sjr@jdns.org>
2025-11-11 10:40:58 -0800

    wast-parser: add support for '(module definition ...)'
    WASM v3 spec tests added the '(module definition ...)' construct to
    WAST. This tells the test runner that the module should not be
    instantiated immediately. The '(module instance ...)' construct (not yet
    supported in wabt) instructs the test runner to instantiate the module.
    
    This change adds an `is_definition` field to ScriptModule. The field
    reflects whether the `definition` keyword was encountered after the
    `module` keyword.
    
    When WastParser encounters a TextModule in a script command, it now
    returns a ScriptModule with the corresponding ScriptTextModule if the
    module is definition-only.
    
    This change allows wabt to parse WASM v3 spec tests that use the
    `(module definition ...)` construct (specifically, `memory.wast`), so
    that test runners built on wabt can support WASM v3 spec tests.

--- include/wabt/ir.h
+++ include/wabt/ir.h
@@ -1368,6 +1368,8 @@ class ScriptModule {
   ScriptModuleType type() const { return type_; }
   virtual const Location& location() const = 0;
 
+  bool is_definition = false;
+
  protected:
   explicit ScriptModule(ScriptModuleType type) : type_(type) {}
 
