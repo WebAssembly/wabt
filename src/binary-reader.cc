a4ec6612cc0bc31bb32887aa38471894bac56e48
odewdney <7547491+odewdney@users.noreply.github.com>
2025-12-02 21:45:19 +0000

    Remove unused includes and print_stacktrace function
    Removed unused includes and stack trace function.

--- src/binary-reader.cc
+++ src/binary-reader.cc
@@ -36,16 +36,6 @@
 #if HAVE_ALLOCA
 #include <alloca.h>
 #endif
-#include <execinfo.h> /* backtrace, backtrace_symbols_fd */
-#include <unistd.h> /* STDOUT_FILENO */
-
-void print_stacktrace(void) {
-   size_t size;
-   enum Constexpr { MAX_SIZE = 1024 };
-   void *array[MAX_SIZE];
-   size = backtrace(array, MAX_SIZE);
-   backtrace_symbols_fd(array, size, STDOUT_FILENO);
-}
 
 #define ERROR(...)           \
   do {                       \
