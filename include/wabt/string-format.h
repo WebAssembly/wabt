ea3dace36735fd5b3551bc71acecbc89b8cda0c5
Lexi Bromfield <lexinadia@google.com>
2026-07-08 18:54:41 +0000

    Use std::abs<int64_t> instead of a hand-rolled implementation.

--- include/wabt/string-format.h
+++ include/wabt/string-format.h
@@ -27,11 +27,9 @@
 #define WABT_PRINTF_STRING_VIEW_ARG(x) \
   static_cast<int>((x).length()), (x).data()
 
-#define PRItypecode "%s%#x"
-#define WABT_PRINTF_TYPE_CODE(x)                                     \
-  (static_cast<int32_t>(x) < 0 ? "-" : ""),                          \
-      (static_cast<int32_t>(x) < 0 ? (0u - static_cast<uint32_t>(x)) \
-                                   : static_cast<uint32_t>(x))
+#define PRItypecode "%s%#lx"
+#define WABT_PRINTF_TYPE_CODE(x) \
+  (static_cast<int32_t>(x) < 0 ? "-" : ""), std::abs(static_cast<int64_t>(x))
 
 #define WABT_DEFAULT_SNPRINTF_ALLOCA_BUFSIZE 128
 #define WABT_SNPRINTF_ALLOCA(buffer, len, format)                          \
